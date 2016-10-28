3828d993e275948fd84d30ba1cbfb39e979c7b93
Clara <clara_fer2@hotmail.com>
2016-10-28 20:36:32 +0200

    Initial commit

---
+++ app/src/main/java/com/clara/ejercicio12/MainActivity.java
@@ -0,0 +1,23 @@
+package com.clara.ejercicio12;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+
+    }
+}
